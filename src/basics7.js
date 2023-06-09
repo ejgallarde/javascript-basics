fd77b9951ed34bec164333f00121c181ab45735f
Earl John Gallarde <earl.john.gallarde@oracle.com>
2023-06-09 14:22:46 +0800

    JS basics

---
+++ src/basics7.js
@@ -0,0 +1,15 @@
+module.exports = class Person {
+  age = 25;
+  get location() {
+    return "Philippines";
+  }
+  //constructor is method which executes by default when you create object of the class
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  //methods
+  fullName() {
+    console.log(this.firstName + this.lastName);
+  }
+};
